ded1ae3c0a14ecc9c36177de024d6b1a37b7da34
Scott Regenthal <scottregenthal@gmail.com>
2019-12-20 10:17:08 -0700

    Begin refactor

---
+++ app/facades/station_search.rb
@@ -0,0 +1,21 @@
+class StationSearch
+  attr_reader :location
+
+  def initialize(location)
+    @location = location
+  end
+
+  def stations
+    conn = Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1/') do |f|
+      f.adapter Faraday.default_adapter
+      f.params['api_key'] = ENV['NREL_API_KEY']
+      f.params['location'] = location
+      f.params['fuel_type'] = 'ELEC'
+    end
+
+    response = conn.get('nearest.json')
+
+    json_parsed = JSON.parse(response.body, symbolize_names: true)
+    @stations = json_parsed[:fuel_stations]
+  end
+end
