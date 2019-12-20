6a96b0d96b3ba41d03586764eed9b864730b7ec7
Scott Regenthal <scottregenthal@gmail.com>
2019-12-20 10:01:57 -0700

    Update search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,17 @@
 class SearchController < ApplicationController
   def index
-    station = params[:station]
+    location = params[:location]
 
     conn = Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1/') do |f|
       f.adapter Faraday.default_adapter
       f.params['api_key'] = ENV['NREL_API_KEY']
-      f.params['location'] = '1331 17th St LL100, Denver, CO 80202'
+      f.params['location'] = location
       f.params['fuel_type'] = 'ELEC'
     end
 
     response = conn.get('nearest.json')
+
+    json_parsed = JSON.parse(response.body, symbolize_names: true)
+    @stations = json_parsed[:fuel_stations]
   end
 end
